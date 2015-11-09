d76f8475383080a1e5a09f27666158fab2b2301f
unixisevil <yujian0231@163.com>
2015-11-09 20:49:35 +0800

    Update bootstrap.sh

--- hadoop/bootstrap.sh
+++ hadoop/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 : ${HADOOP_PREFIX:=/usr/local/hadoop}
 
@@ -9,18 +10,27 @@ $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 rm /tmp/*.pid
 #host_ip=$(ifconfig eth0|awk -F '[: ]*' '/inet addr/{print $4}')
 
+if [ -z $HADOOP_MASTER ];then
+    echo "HADOOP_MASTER not set" && exit 1
+fi
+
+if [ -z $HADOOP_SLAVE ];then
+    echo "HADOOP_SLAVE not set" && exit 1
+fi
+
 sed -i  s/master/$HADOOP_MASTER/  \
         $HADOOP_CONF_DIR/core-site.xml  \
-        $HADOOP_CONF_DIR/mapred-site.xml \
         $HADOOP_CONF_DIR/yarn-site.xml
 
 service sshd start
 
 if  [ $role = "master" ]; then
+
+    sed -i  s/master/$HADOOP_MASTER/   $HADOOP_CONF_DIR/mapred-site.xml
     echo > $HADOOP_CONF_DIR/slaves
-	hdfs namenode -format
-	start-dfs.sh
-	start-yarn.sh
+    echo y | hdfs namenode -format
+        start-dfs.sh
+        start-yarn.sh
     while true;do
            sleep 5
            if [ -z $HADOOP_SLAVE ];then
@@ -30,7 +40,7 @@ if  [ $role = "master" ]; then
                echo  $s  >> $HADOOP_CONF_DIR/slaves
            done
            content=$(cat $HADOOP_CONF_DIR/slaves|sort -u)
-           echo "$content"  >  $HADOOP_CONF_DIR/slaves
+           echo -e  "$HADOOP_MASTER\n$content"  >  $HADOOP_CONF_DIR/slaves
     done
 fi
 
